111c70a3072441910b89e76818175b7dd3e4b6f9
JuanCarlos <jcgpalacios1@gmail.com>
2025-09-26 22:00:29 -0500

    First commit

---
+++ actions.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const texto = "Juan Carlos Gorriti";
+    const span = document.getElementById("nombre");
+    let i = 0;
+  
+    function escribir() {
+      if (i < texto.length) {
+        span.textContent += texto.charAt(i);
+        i++;
+        setTimeout(escribir, 120); // velocidad del typing
+      }
+    }
+  
+
+    setTimeout(escribir, 1000);
+  });
+  
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const desc = "Desarrollador fullstack con experiencia en Java, Spring Boot, Angular y bases de datos. Me apasiona crear soluciones digitales modernas, escalables y con buena experiencia de usuario.";
+    const span = document.getElementById("descripcion");
+    let i = 0;
+
+    function escribir(){
+        if(i<desc.length){
+            span.textContent += desc.charAt(i);
+            i++;
+            setTimeout(escribir,25);
+        }
+    }
+
+    setTimeout(escribir, 100)
+})
