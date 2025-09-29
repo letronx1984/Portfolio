10b2be8b9842f8143df4bcbee5b450b77ab92cd5
JuanCarlos <jcgpalacios1@gmail.com>
2025-09-29 02:51:36 -0500

    portfolio v2

--- js/actions.js
+++ js/actions.js
@@ -1,34 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const texto = "Juan Carlos Gorriti";
-    const span = document.getElementById("nombre");
-    let i = 0;
-  
-    function escribir() {
-      if (i < texto.length) {
-        span.textContent += texto.charAt(i);
-        i++;
-        setTimeout(escribir, 120); // velocidad del typing
-      }
+  const texto = "Juan Carlos Gorriti";
+  const span = document.getElementById("nombre");
+
+  let i = 0;
+
+
+  const cursor = document.createElement("span");
+  cursor.classList.add("cursor");
+  span.after(cursor);
+
+  function escribir() {
+    if (i < texto.length) {
+      span.textContent += texto.charAt(i);
+      i++;
+      setTimeout(escribir, 120); 
     }
-  
+  }
 
-    setTimeout(escribir, 1000);
-  });
-  
+  setTimeout(escribir, 1000);
+});
 
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
+  const desc =
+    "Desarrollador backend con experiencia en Java, Spring Boot, Angular y bases de datos. Me apasiona crear soluciones digitales modernas, escalables y con buena experiencia de usuario.";
+  const span = document.getElementById("descripcion");
 
-    const desc = "Desarrollador backend con experiencia en Java, Spring Boot, Angular y bases de datos. Me apasiona crear soluciones digitales modernas, escalables y con buena experiencia de usuario.";
-    const span = document.getElementById("descripcion");
-    let i = 0;
+  let i = 0;
 
-    function escribir(){
-        if(i<desc.length){
-            span.textContent += desc.charAt(i);
-            i++;
-            setTimeout(escribir,25);
-        }
+
+  const cursor = document.createElement("span");
+  cursor.classList.add("cursor");
+  span.after(cursor);
+
+  function escribir() {
+    if (i < desc.length) {
+      span.textContent += desc.charAt(i);
+      i++;
+      setTimeout(escribir, 25); 
     }
+  }
 
-    setTimeout(escribir, 100)
-})
+  setTimeout(escribir, 100);
+});
